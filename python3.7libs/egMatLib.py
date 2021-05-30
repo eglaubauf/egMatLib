fa61d4f02b774d54276ee618cc9e4478d598340d
eglaubauf <contact@elmar-glaubauf.at>
2021-05-30 10:38:12 +0200

    Basic Import Export

--- python3.7libs/egMatLib.py
+++ python3.7libs/egMatLib.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import hou
+import time
 
 # PySide2
 from PySide2.QtGui import *
@@ -9,28 +10,33 @@ from PySide2.QtCore import *
 from PySide2 import QtUiTools
 
 
-class egMatLibPanel(QtWidgets.QWidget):
+class egMatLibPanel(QWidget):
     def __init__(self):
         super(egMatLibPanel, self).__init__()
         self.load_settings()
         scriptpath = os.path.dirname(os.path.realpath(__file__))
 
+        # Config
+        self.thumbSize = 150
+        self.extension = ".rsmat"
+        self.lib = os.path.dirname("G:/Git/egMatLib/lib/")
+
         ## Load UI from ui.file
-        loader = QUiLoader()
-        file = QtCore.QFile(scriptpath + '/MatLib.ui')
-        file.open(QtCore.QFile.ReadOnly)
+        loader = QtUiTools.QUiLoader()
+        file = QFile(scriptpath + '/MatLib.ui')
+        file.open(QFile.ReadOnly)
         self.ui = loader.load(file)
         file.close()
 
         # Load Ui Element so self
         self.menu = self.ui.findChild(QMenuBar, 'menubar')
 
-        # Config
-        self.thumbSize = 150
-
         # Link Buttons
         self.btn_update = self.ui.findChild(QPushButton, 'btn_update')
         self.btn_update.clicked.connect(self.loadMaterials)
+
+        self.btn_save = self.ui.findChild(QPushButton, 'btn_save')
+        self.btn_save.clicked.connect(self.save_material)
 
         self.btn_delete = self.ui.findChild(QPushButton, 'btn_delete')
         self.btn_delete.clicked.connect(self.delete_material)
@@ -43,7 +49,7 @@ class egMatLibPanel(QtWidgets.QWidget):
         self.thumblist.setIconSize(QSize(self.thumbSize, self.thumbSize))
         self.thumblist.setSpacing(1)
         #self.thumblist.setFlow()
-        #self.thumblist.doubleClicked.connect(self.setTexture)
+        self.thumblist.doubleClicked.connect(self.import_material)
         #self.thumblist.clicked.connect(self.setLargePreview)
         #self.thumblist.installEventFilter(self)
 
@@ -56,26 +62,23 @@ class egMatLibPanel(QtWidgets.QWidget):
         self.create_default_icon()
         self.loadMaterials()
 
+
     def loadMaterials(self):
         # TODO: Load from Settings.json
-        mat_dir = os.path.dirname("G:/Git/egMatLib/lib/")
+
         self.files = []
-        for f in os.listdir(mat_dir):
-            curr = os.path.join(mat_dir,f)
+        for f in os.listdir(self.lib):
+            curr = os.path.join(self.lib,f)
             if os.path.isfile(curr):
-                curr = mat_dir + '/' + f
-                if curr.endswith('.rsmat'):
+                curr = self.lib + '/' + f
+                if curr.endswith(self.extension):
                     self.files.append(curr)
 
-
-
         #  Now get all images for our Materials and List add them to the thumblist
-
         #  Dirty Empty for now
         self.thumblist.clear()  # TODO: Make nice and fast in the future by only changing new ones
-
         for f in self.files:
-            img = f.replace('.rsmat', '.png')
+            img = f.replace(self.extension, '.png')
 
             # Check if Thumb Exists and attach
             if os.path.isfile(img):
@@ -87,6 +90,8 @@ class egMatLibPanel(QtWidgets.QWidget):
             # Create entry in Thumblist
             img_name = img.rsplit('/', 1)[1][:-4]
             item = QListWidgetItem(icon, img_name)
+            #item.addRoleName()
+            #item.setData(Qt.UserRole, f)  # add Filepath to Item
             item.setTextAlignment(Qt.AlignHCenter | Qt.AlignBottom)
             self.thumblist.addItem(item)
             self.thumblist.sortItems()
@@ -123,8 +128,83 @@ class egMatLibPanel(QtWidgets.QWidget):
             print('Deleted')
         return
 
-    def import_material(self):
-        pass
+    def import_material(self, item):
 
+        file_name = self.lib + "/" + item.data() + self.extension
+
+        # CreateBuilder
+        builder = hou.node('/mat').createNode('redshift_vopnet')
+        builder.setName(item.data(), unique_name=True)
+
+        # Delete Default children in RS-VopNet
+        for node in builder.children():
+            node.destroy()
+        # Load File
+        builder.loadItemsFromFile(file_name, ignore_load_warnings=False)
+        # MakeFancyPos
+        builder.moveToGoodPosition()
+        return
+
+
+    #  Saves a Material to the Library
     def save_material(self):
-        pass
+
+        sel = hou.selectedNodes()
+
+        # Check selection
+        if not sel:
+            hou.ui.displayMessage('No Material selected')
+            return
+
+        # Get Material Builder Node
+        matBuild = sel[0] # TODO Multiple Nodes at once?
+
+        # Check against NodeType
+
+        if matBuild.type().name() != "redshift_vopnet":
+            hou.ui.displayMessage('Selected Node is not a Material Builder')
+            return
+
+        # Filepath where to save stuff
+        name = matBuild.name()
+        file_name = self.lib + "/" + name + self.extension
+
+        children = matBuild.children()
+        matBuild.saveItemsToFile(children, file_name, save_hda_fallbacks=False)
+
+        # Create Thumbnail
+        thumb = hou.node("/obj").createNode("eg_thumbnail")
+
+        thumb.parm("mat").set(matBuild.path())
+
+        # Build path
+        path = self.lib + '/' + name + ".png"
+        #  Set Rendersettings and Object Exclusions for Thumbnail Rendering
+        thumb.parm("path").set(path)
+        exclude = "* ^" + thumb.name()
+        thumb.parm("obj_exclude").set(exclude)
+        lights = thumb.name() + "/*"
+        thumb.parm("lights").set(lights)
+        thumb.parm("render").pressButton()
+
+        # Wait until Render is finished
+        self.waitForRender(path)
+
+        # CleanUp
+        thumb.destroy()
+
+        # Reload Library
+        self.loadMaterials()
+
+        # OK, bye
+        msg = "Material " + name + " created"
+        hou.ui.displayMessage(msg)
+        return
+
+    def waitForRender(self, path):
+        mustend = time.time() + 60.0
+        while time.time() < mustend:
+            if os.path.exists(path):
+                return True
+            time.sleep(0.5)
+        return False
