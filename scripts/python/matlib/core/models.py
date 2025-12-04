9ae571198568fdace648cef23ea793ddffb335bc
egx <a@b.c>
2025-12-04 21:51:42 +0100

    fix type annotation

--- scripts/python/matlib/core/models.py
+++ scripts/python/matlib/core/models.py
@@ -32,7 +32,7 @@ class MaterialLibrary:
 
         self._context: hou.Node = hou.node("/stage")
 
-    def load(self, path: str, prefs: prefs) -> None:
+    def load(self, path: str, prefs: prefs.Prefs) -> None:
         self._path = path
         with open(self._path + ("/library.json"), encoding="utf_8") as lib_json:
             self._data = json.load(lib_json)
