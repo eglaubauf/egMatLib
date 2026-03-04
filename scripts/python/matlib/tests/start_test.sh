373049aa2caa51ac3b7793801e2e7591bd2424b7
egx <e@g.x>
2026-03-04 22:41:11 +0100

    enable all tests

--- scripts/python/matlib/tests/start_test.sh
+++ scripts/python/matlib/tests/start_test.sh
@@ -2,17 +2,17 @@
 cd /opt/hfs21.0
 source houdini_setup
 cd /home/elmar/git/egMatLib/scripts/python/matlib/tests
-# hython -m unittest tests
 
-# echo "---------------------------"
-# echo "Testing Category Module"
-# echo "---------------------------"
-# hython -m unittest test_category
 
-# echo "---------------------------"
-# echo "Testing Worker Module"
-# echo "---------------------------"
-# hython -m unittest test_worker
+echo "---------------------------"
+echo "Testing Category Module"
+echo "---------------------------"
+hython -m unittest test_category
+
+echo "---------------------------"
+echo "Testing Worker Module"
+echo "---------------------------"
+hython -m unittest test_worker
 
 echo "---------------------------"
 echo "Testing Library Module"
